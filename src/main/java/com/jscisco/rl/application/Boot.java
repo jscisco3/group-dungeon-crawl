ae7788b8817ea07cc1c89e7e729bab2ea537b44c
Jack Scisco <jackscisco@gmail.com>
2021-07-30 19:47:58 -0400

    feature(libgdx): Initializes project to use libgdx

---
+++ src/main/java/com/jscisco/rl/application/Boot.java
@@ -0,0 +1,10 @@
+package com.jscisco.rl.application;
+
+import com.badlogic.gdx.Game;
+
+public class Boot extends Game {
+    @Override
+    public void create() {
+
+    }
+}
---
+++ src/main/java/com/jscisco/rl/application/DesktopLauncher.java
@@ -0,0 +1,19 @@
+package com.jscisco.rl.application;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setIdleFPS(60);
+        config.useVsync(true);
+        config.setTitle("Party Roguelike");
+
+        config.setWindowedMode(960, 640);
+
+        new Lwjgl3Application(new Boot(), config);
+    }
+
+}
